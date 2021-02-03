374aea7c7df8257538d6ceb90fa50ac46907d3ea
CharlotteMoureau <Charlotte.Moureau96@gmail.com>
2021-02-03 12:01:41 +0100

    12.01

---
+++ 01-base/04-asv-confirm/script.js
@@ -0,0 +1,26 @@
+/* becode/javascript
+ *
+ * /01-base/04-asv-confirm/script.js - 1.4: ASV avec confirmation
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function () {
+
+    var age = prompt("How old are you?");
+    var gender = prompt("What is your gender?");
+    var town = prompt("Where do you live?");
+
+    var x = confirm(`Is it correct? You are ${age}, you identify as: ${gender}, you live in ${town}.`);
+    if (x == true) {
+        window.alert("Ok!")
+    } else {
+        location.reload()
+    }
+
+
+})();
