b723c92617d51334733d737b0b1c29aa33f632b5
CharlotteMoureau <Charlotte.Moureau96@gmail.com>
2021-02-05 14:59:39 +0100

    14:59

--- 04-dates/01-title-by-hour-one/script.js
+++ 04-dates/01-title-by-hour-one/script.js
@@ -9,10 +9,18 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
+    let d = new Date();
+    let hour = d.getHours();
+    console.log(hour);
+
+    if (hour <= 18)
+        document.getElementById("target").innerHTML = "Hello!";
+    else
+        document.getElementById("target").innerHTML = "Good evening!";
 
 })();
