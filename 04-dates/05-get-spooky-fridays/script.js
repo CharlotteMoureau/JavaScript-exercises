77383f020ea1f55ab0eeef7e829f1e716c521ac4
CharlotteMoureau <Charlotte.Moureau96@gmail.com>
2021-02-08 14:34:21 +0100

    14:34

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -9,8 +9,24 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
+    document.getElementById("run").addEventListener("click", function () {
 
+        function numberOfFridaythe13thsIn(year) {
+            var count = 0;
+            for (var month = 0; month < 12; month++) {
+                var d = new Date(year, month, 13);
+                if (d.getDay() == 5) {
+                    let preciseMonth = d.toLocaleString('en-EN', { month: 'long' });
+                    console.log(preciseMonth);
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        alert(`There is ${numberOfFridaythe13thsIn(document.getElementById("year").value)} Friday the 13th in ${document.getElementById("year").value} (see the console to know which month).`);
+
+    });
 })();
