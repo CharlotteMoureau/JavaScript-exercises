bdd18884fc2f5ce2092be774593afd5692294822
CharlotteMoureau <Charlotte.Moureau96@gmail.com>
2021-02-23 09:12:00 +0100

    save

--- 09-misc/01-waving-text/script.js
+++ 09-misc/01-waving-text/script.js
@@ -12,5 +12,21 @@
 (function () {
 
     // your code here
+    let target = document.getElementById('target').textContent;
+    let wave = [];
+    wave.push(target.slice(0, 8));
+    wave.push(target.slice(9, 26));
+    wave.push(target.slice(27, 33));
+    wave.push(target.slice(34, 40));
+    wave.push(target.slice(41,));
 
+    document.getElementById('target').innerHTML = '';
+
+    for (let i = 0; i < wave.length; i++) {
+        let randomEm = Math.floor(Math.random() * 4);
+        let p = document.createElement('p');
+        p.setAttribute('style', `font-size : ${randomEm}em ; display: inline;`)
+        p.innerHTML = wave[i];
+        document.getElementById('target').appendChild(p);
+    }
 })();
