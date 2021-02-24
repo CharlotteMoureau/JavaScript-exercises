3ef4f230bb4d02a66edae8b43b3bb686a016dab7
CharlotteMoureau <Charlotte.Moureau96@gmail.com>
2021-02-24 17:29:11 +0100

    17:29

--- 11-fetch/03-details/script.js
+++ 11-fetch/03-details/script.js
@@ -11,4 +11,29 @@
 
 (() => {
     // your code here
+    const clone = document.querySelector('#tpl-hero').cloneNode(true).content;
+    document.querySelector('#target').appendChild(clone);
+
+    async function test() {
+        try {
+            const myFetch = await fetch('http://localhost:3000/heroes');
+            const data = await myFetch.json();
+
+            data.forEach(element => {
+                const input = document.querySelector('#hero-id').value;
+
+                if (element.id == input) {
+                    document.querySelector('.name').innerHTML = element.name;
+                    document.querySelector('.alter-ego').innerHTML = element.alterEgo;
+                    document.querySelector('.powers').innerHTML = element.abilities.join('. ');
+                }
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    document.getElementById('run').addEventListener('click', function () {
+        test();
+    });
 })();
--- 11-fetch/04-add/script.js
+++ 11-fetch/04-add/script.js
@@ -11,4 +11,37 @@
 
 (() => {
     // your code here
+    async function aNewHeroAppears() {
+        try {
+            const myFetch = await fetch('http://localhost:3000/heroes');
+            const data = await myFetch.json();
+
+            console.log(data);
+
+            const nameInput = document.querySelector('#hero-name').value;
+            const alterEgoInput = document.querySelector('#hero-alter-ego').value;
+            const powersInput = document.querySelector('#hero-powers').value;
+
+            if (nameInput.length != 0 && alterEgoInput.length != 0 && powersInput.length != 0) {
+                let i = data.length + 1;
+                i++;
+                const newHero = {
+                    id: i,
+                    name: nameInput,
+                    alterEgo: alterEgoInput,
+                    abilities: [powersInput],
+                }
+                data.push(newHero);
+                console.log(data);
+
+            } else {
+                alert('There seems to be an empty input');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    document.getElementById('run').addEventListener('click', function () {
+        aNewHeroAppears();
+    });
 })();
